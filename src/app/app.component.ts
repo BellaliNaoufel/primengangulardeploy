957b841e486693f821e2bdb3d53ab0c1e1167b8f
Naoufel Bellali <naoufel.bellali@external.total.com>
2020-09-07 20:46:32 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { MenuItem } from 'primeng/api';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'CatMash';
+  items: MenuItem[];
+
+  ngOnInit(): void {
+    this.items = [
+      {
+        label: 'Sondage',
+        icon: 'pi pi-fw pi-align-justify',
+        items: [{
+          label: 'Meilleur chat',
+          icon: 'pi pi-fw pi-star',
+          items: [
+            { label: 'Voter', icon: 'pi pi-fw pi-star', routerLink: ['/catvote'] },
+            { label: 'Voir le classement', icon: 'pi pi-fw pi-sort-amount-down', routerLink: ['/catlist'] }
+          ]
+        }
+        ]
+      },
+      {
+        label: 'Paramètres',
+        icon: 'pi pi-fw pi-cog',
+        items: [{
+          label: 'Réinitialiser',
+          icon: 'pi pi-fw pi-arrow-circle-down'
+        }
+        ]
+    }
+    ];
+  }
+}
